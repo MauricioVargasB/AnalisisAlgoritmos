93eb1c7b5f9290001436ab5d69099a7441664d96
MauricioVargasB <41761276+MauricioVargasB@users.noreply.github.com>
2018-08-24 07:51:47 -0500

    Update algoritmosTubos.py

--- ProgramacionDinamica/algoritmosTubos.py
+++ ProgramacionDinamica/algoritmosTubos.py
@@ -1,1 +1,19 @@
-#sfdsdfsfadfsd
+#Created by Mauricio Vargas Ballesteros & Carlos Rodriguez
+
+#faltan inicializar los arreglos :(
+
+r[0]=0
+for i in range (0:m)
+  for j in range (1:i)
+  v=P[j]+r[i-j]
+  if(r[i]<v)
+    r[i]=v
+    s[i]=k
+m=n
+t=0
+while m>0:
+  t=t+s[m]
+  m=m-s[m]
+
+print('r[n]: ' + str(r[n]))
+print ('T: '+ str(t))
